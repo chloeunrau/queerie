d9daecda05a10ce755e1401728e7db84d11263ea
Chloe Unrau <chloe.unrau@gmail.com>
2015-10-04 13:12:02 -0700

    Complete the footer content
    Adds content to the footer and makes it responsive.

--- footer.php
+++ footer.php
@@ -2,6 +2,40 @@
 <div class="clearfix"></div>
 
 <footer>
+
+  <div id="footer-top"></div>
+
+  <div id="footer-bottom">
+
+    <div id="footer-left">
+      <div id="footer-logo">
+        <img src="<?php echo bloginfo('template_url'); ?>/images/sm-horizontal-logo.png" />
+      </div>
+    </div>
+
+    <div id="footer-content">
+      <p class="footer-link-row">
+        <a href="">LISTEN NOW</a><a href="">DONATE</a>
+      </p>
+      <p class="footer-link-row">
+        <a href="">HOME</a><a href="">ABOUT</a><a href="">ARCHIVE</a><a href="">CONTACT</a>
+      </p>
+      <p class="footer-link-row">
+        <a href="">FACEBOOK</a><a href="">TWITTER</a>
+      </p>
+      <p class="footer-link-row">
+        <a href="">CO-OP RADIO</a>
+      </p>
+    </div>
+
+    <div id="footer-right">
+      <div id="footer-coop">
+        <img src="<?php echo bloginfo('template_url'); ?>/images/sm-horizontal-coop.png" />
+      </div>
+    </div>
+
+  </div>
+
 </footer>
 
 </div> <!-- #content -->
